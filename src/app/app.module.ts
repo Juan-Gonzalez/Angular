605af8af5c4c507926ad6860d3251e00f61f58a9
Juan-Gonzalez <gonzalezd.juan@gmail.com>
2017-06-26 20:09:29 +1000

    why is not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -9,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+  ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
